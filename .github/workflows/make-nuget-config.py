a4449718455b8c87b04eea69f0260bec82639e96
David Maas <contact@pathogenstudios.com>
2020-08-02 04:33:58 -0500

    Added NuGet.org test

--- .github/workflows/make-nuget-config.py
+++ .github/workflows/make-nuget-config.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import io
 import os
+import sys
 
 config_file_path = "NuGet.config"
 
 assert(not os.path.exists(config_file_path)), f"{config_file_path} must not already exist!"
+
+if os.environ.get('NUGETTEST') == 'true':
+    with io.open(config_file_path, mode="w") as f:
+        f.write(f"""<configuration>
+    <packageSources>
+        <clear />
+        <add key="nugettest" value="https://int.nugettest.org/api/v2/" />
+    </packageSources>
+</configuration>
+""")
+    sys.exit(0)
 
 owner = os.environ['GITHUB_REPOSITORY_OWNER']
 token = os.environ['GITHUB_TOKEN']
